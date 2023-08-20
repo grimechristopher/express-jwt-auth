745446d7f9dd4fa88742e1b76187f67415e93f1b
Christopher Grime <chris@chrisgrime.com>
2023-08-20 15:38:01 -0700

    Create the Account Model.

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,1 +1,23 @@
 // account.model.js
+import pool from "../config/database.config.js";
+
+class Account{
+  async findByEmail(email) {
+    // return array of accounts matching an email
+    let { rows } = await pool.query(`SELECT * FROM account WHERE email = '${email}';`);
+    return rows
+  }
+  async createAccount(payload) {
+    // Check to make sure an account with the same email doesnt exist
+    let emails = await pool.query(`SELECT * FROM account WHERE email = '${payload.email}';`);
+    if (emails.rows.length) {
+      return new Error('Email already exists')
+    }
+
+    // insert new account into the database
+    return await pool.query(`INSERT INTO account (email, password) VALUES ('${payload.email}', '${payload.password}');`)
+  }
+}
+
+// export the Account class
+export default Account;
