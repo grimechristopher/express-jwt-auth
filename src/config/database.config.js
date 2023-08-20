3dacf229609073876a60dbbfd70f32ea3a8eec43
Christopher Grime <chris@chrisgrime.com>
2023-08-20 15:14:52 -0700

    Update database config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -5,9 +5,12 @@ const { Pool } = pg
 const credentials = {
   user: "postgres",
   host: "localhost",
-  database: "jwt-auth-sample",
+  database: "jwt_auth_sample",
   password: process.env.PG_PASSWORD,
   port: 5432,
 };
 
+// Connect to the database
 const pool = new Pool(credentials);
+
+export default pool;
