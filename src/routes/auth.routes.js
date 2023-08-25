3050d6c6f42442b3079e7b9e3a8b50ee33fa5056
Christopher Grime <chris@chrisgrime.com>
2023-08-24 20:56:54 -0700

    Auth Routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,1 +1,29 @@
-// auth.routes.js
+// authRouter.routes.js
+import express from 'express';
+
+import verifyAuth from '../middleware/verifyAuth.middleware.js';
+import AccountController from '../controllers/account.controller.js';
+
+let authRouter = express.Router();
+let accountController = new AccountController()
+
+authRouter.post('/signin/', function(request, response, next){
+  accountController.signIn(request, response, next)
+});
+
+authRouter.post('/signup/', function(request,response, next) {
+  accountController.signUp(request, response, next)
+});
+
+authRouter.get('/test-public/' , function(request,response) {
+  accountController.testPublicRequest(request, response)
+});
+
+authRouter.get('/test-private/', verifyAuth, function(request,response) {
+  accountController.testPrivateRequest(request, response)
+});
+
+
+
+
+export default authRouter;
