12c350c471557261b1bf9810c5dade45b915177d
Christopher Grime <chris@chrisgrime.com>
2023-08-24 20:57:36 -0700

    Unit tests for all things account related

--- test/account.test.js
+++ test/account.test.js
@@ -18,6 +18,9 @@ import * as dotenv from 'dotenv';
 
 // Import files being tested
 import AccountModel from '../src/models/account.model';
+import AccountController from '../src/controllers/account.controller';
+
+import authRouter from '../src/routes/auth.routes.js';
 
 // Initialize variables
 let server = null;
@@ -34,9 +37,16 @@ jest.mock('pg', () => {
   return {Pool: jest.fn(() => mPool)};
 });
 
-// function flushPromises() {
-//   return new Promise(resolve => setImmediate(resolve));
-// }
+const req = {
+  body: {
+    email: "test@example.com",
+    password: "Test",
+  }
+};
+const res = {
+  send: jest.fn()
+}
+const next = jest.fn();
 
 // Functions that need to run before any tests are ran 
 beforeAll(() => {
@@ -45,7 +55,7 @@ beforeAll(() => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(cookieParser());
-  // app.use('/auth', authRouter);
+  app.use('/auth', authRouter);
 
   server = app.listen();
   pool = new Pool();
@@ -94,12 +104,9 @@ describe('Account Model', () => {
       });
   
       const email = 'chris@chrisgrime.com'
-      
       const findingEmail = await accountModel.findByEmail(email);
-  
       expect(pool.query).toBeCalledTimes(1);
       expect(pool.query).toHaveBeenCalledWith("SELECT * FROM account WHERE email = 'chris@chrisgrime.com';");
-      
       expect(findingEmail).toHaveLength(1);
       expect(findingEmail).toEqual(
         expect.arrayContaining([expect.objectContaining({email: 'chris@chrisgrime.com'}),]
@@ -144,6 +151,194 @@ describe('Account Model', () => {
       expect(pool.query).toBeCalledTimes(2);
       expect(pool.query).toHaveBeenCalledWith("SELECT * FROM account WHERE email = 'chris@chrisgrime.com';");  
       expect(pool.query).toHaveBeenCalledWith("INSERT INTO account (email, password) VALUES ('chris@chrisgrime.com', 'test');");  
+    });
+  });
+});
+describe("Account Controller", () => {
+  describe("signup()", () => {
+    test("rejects missing email", async () => {
+      let body = {
+        password: 'test',
+      };
+  
+      // Response should be a 400 Bad Request error with a message explaining why
+      const response = await request(app).post('/auth/signup').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toEqual('Missing email');
+    });
+  
+    test("rejects missing password", async () => {
+      let body = {
+        email: 'test@example.com',
+      };
+  
+      const response = await request(app).post('/auth/signup').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toEqual('Missing password');
+    });
+  
+    test("creates query for account and returns success", async () => {
+      let body = {
+        email: 'test@example.com',
+        password: 'test',
+      };
+  
+      const response = await request(app).post('/auth/signup').send(body); 
+  
+      expect(pool.query).toBeCalledTimes(2);
+      expect(pool.query).toHaveBeenCalledWith("SELECT * FROM account WHERE email = 'test@example.com';");  
+      expect(pool.query).toHaveBeenCalledWith(expect.stringMatching(/INSERT INTO account \(email, password\) VALUES \('test@example.com', '[^']+'\);/));
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(201);
+      expect(response.text).toEqual('Account created successfully');
+    });
+
+    test("does not call database with same password value as passed in", async () => {
+      let body = {
+        email: 'test@example.com',
+        password: 'test',
+      };
+  
+      const response = await request(app).post('/auth/signup').send(body); 
+  
+      expect(pool.query).toBeCalledTimes(2);
+      expect(pool.query).toHaveBeenCalledWith("SELECT * FROM account WHERE email = 'test@example.com';");  
+      expect(pool.query).not.toHaveBeenCalledWith("INSERT INTO account (email, password) VALUES ('test@example.com', 'test');");
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(201);
+      expect(response.text).toEqual('Account created successfully');
+    });
+    
+    
+    test("The password is hashed before being inserted into the database", async () => {
+      const accountController = new AccountController();
+      jest.spyOn(accountController.account, 'createAccount');
+      await accountController.signUp(req, res, next);
+      expect(accountController.account.createAccount).toHaveBeenCalled();
+      expect(accountController.account.createAccount).toHaveBeenCalledWith(
+        {"email": "test@example.com", "password":expect.stringMatching(/^\$2b\$10\$.{53}$/)}
+      );
+    });
+    
+  });
+  describe("signin()", () => {
+    test("rejects missing email", async () => {
+      let body = {
+        password: 'test',
+      };
+  
+      const response = await request(app).post('/auth/signin').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toEqual('Missing email');
+    });
+  
+    test("signin() rejects missing password", async () => {
+      let body = {
+        email: 'chris@chrisgrime.com',
+      };
+  
+      const response = await request(app).post('/auth/signin').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toEqual('Missing password');
+    });
+  
+  
+    test('signin() returns error if password is incorrect', async () => {
+      let body = {
+        email: 'chris@chrisgrime.com',
+        password: 'test',
+      };
+  
+      pool.query.mockResolvedValue({
+        rows: [
+          {
+            email: 'chris@chrisgrime.com',
+            password: 'test',
+          },
+        ],
+      });
+  
+      const response = await request(app).post('/auth/signin').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(403);
+      expect(response.text).toEqual('Incorrect password');
+    });
+  
+    test('signin() successfully authenticates user', async () => {
+      let body = {
+        email: 'chris@chrisgrime.com',
+        password: 'test',
+      };
+  
+      pool.query.mockResolvedValue({
+        rows: [
+          {
+            email: 'chris@chrisgrime.com',
+            password: await bcrypt.hash('test',10),
+          },
+        ],
+      });
+  
+      const response = await request(app).post('/auth/signin').send(body); 
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.header['set-cookie'][0]).toEqual(expect.stringMatching(/jwt-auth=[^']+HttpOnly; Secure; SameSite=Lax/));
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toEqual('Account logged in');      
+    });
+  });
+  describe("testPublicRequest()", () => {
+    test('response success', async () => {
+      const response = await request(app).get('/auth/test-public'); 
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toEqual('Success');      });
+  });
+  describe("testPrivateRequest()", () => {
+    test('returns error without token', async () => {
+      const response = await request(app).get('/auth/test-private');
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(403);
+      expect(response.text).toEqual('No access token provided');      });
+  
+    test('returns error with incorrect token', async () => {    
+      const response = await request(app).get('/auth/test-private').set('Cookie', ["jwt-auth=wrong"]); 
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(403);
+      expect(response.text).toEqual('Invalid access token');
+    });
+  
+    test('returns response succesfully', async () => {
+      let body = {
+        email: 'chris@chrisgrime.com',
+        password: 'test',
+      };
+  
+      pool.query.mockResolvedValue({
+        rows: [
+          {
+            email: 'chris@chrisgrime.com',
+            password: await bcrypt.hash('test',10),
+          },
+        ],
+      });
+  
+      const signInResponse = await request(app).post('/auth/signin').send(body); 
+      let cookie = signInResponse.get('Set-Cookie')
+  
+      const response = await request(app).get('/auth/test-private').set('Cookie', [...cookie]); 
+  
+      expect(response.header['content-type']).toBe('text/html; charset=utf-8');
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toEqual('Success with access token');
     });
   });
 });
